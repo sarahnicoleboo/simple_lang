b912f758dab1de8e9f96a23c9a412ebc1aa337be
Sarah Canto <sarah.cantohyatt.5@my.csun.edu>
2022-10-25 19:15:17 -0700

    added syntax

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 simple expression language we're using:
 
-x is a variable
-i is an integer
+x is a variable literal
+i is an integer literal
+bool is a boolean value (true or false)
+str is a string literal
 
-type ::= int | bool </br >
-primary_exp ::= x | i | '(' exp ')' </br >
+type ::= int | bool | string</br >
+primary_exp ::= x | i | bool | str </br >
 additive_op ::= + | - </br >
 additive_exp ::= primary_exp (additive_op primary_exp)* </br >
 comparison_op ::= < | > </br >
 comparison_exp ::= additive_exp (comparison_op additive_exp)* </br >
-ex_equals_exp ::= comparison_exp ('==' comparison_exp)* </br >
-exp ::= ex_equals_exp </br >
-vardec ::= type x </br >
-stmt ::= vardec = exp; | if (exp) stmt else stmt | { stmt* } </br >
+equals_op ::= == | !=
+equals_exp ::= comparison_exp (equals_op comparison_exp)* </br >
+exp ::= equals_exp </br >
+stmt ::= type x = exp; | </br >
+	 x = exp; | </br >
+	 if (exp) stmt else stmt | </br >
+	 while (exp) stmt | </br >
+	 print(exp*) | </br >
+	 { stmt* } | </br >
+	 exp </br >
 program ::= stmt
--- src/main/scala/simple_lang_typechecker/typechecker/syntax.scala
+++ src/main/scala/simple_lang_typechecker/typechecker/syntax.scala
@@ -1,1 +1,54 @@
 package simple_lang_typechecker.typechecker
+
+//types
+sealed trait Type
+case object IntType extends Type
+case object BoolType extends Type
+case object StringType extends Type
+
+//operators
+sealed trait Op
+sealed trait AdditiveOp extends Op
+case object PlusOP extends AdditiveOp
+case object MinusOp extends AdditiveOp
+
+sealed trait ComparisonOp extends Op
+case object LessThanOp extends ComparisonOp
+case object GreaterThanOp extends ComparisonOp
+
+sealed trait EqualsOp extends Op
+case object ExactlyEqualsOp extends EqualsOp
+case object NotEqualsOp extends EqualsOp
+//end of operators
+
+case class Variable(name: String)
+
+//expressions
+sealed trait Exp
+//primary exps
+sealed trait PrimaryExp extends Exp
+case class VariableExp(name: Variable) extends PrimaryExp
+case class IntegerLiteralExp(value: Int) extends PrimaryExp
+case class BooleanLiteralExp(value: Boolean) extends PrimaryExp
+case class StringLiteralExp(value: String) extends PrimaryExp
+
+case class AdditiveExp(left: PrimaryExp, op: AdditiveOp, right: PrimaryExp) extends Exp
+case class ComparisonExp(left: AdditiveExp, op: ComparisonOp, right: AdditiveExp) extends Exp
+case class EqualsExp(left: ComparisonExp, op: EqualsOp, right: ComparisonExp) extends Exp
+//end of exps
+
+//stmts
+sealed trait Stmt
+case class VariableDeclarationStmt(theType: Type, name: Variable, exp: Exp) extends Stmt
+case class VariableAssignmentStmt(name: Variable, exp: Exp) extends Stmt
+case class IfStmt(guard: Exp, ifTrue: Stmt, ifFalse: Stmt) extends Stmt
+case class WhileStmt(guard: Exp, body: Stmt) extends Stmt
+case class PrintStmt(exps: Seq[Exp]) extends Stmt
+case class BlockStmt(smts: Seq[Stmt]) extends Stmt
+case class ExpStmt(exp: Exp) extends Stmt
+
+case class Program(stmt: Stmt)
+
+
+
+
